3d0edc791c81ff0087c718e51407e64758d92c00
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-10-24 12:58:29 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
 import json
 
 def lambda_handler(event, context):
+    # Hardcoded credentials (Insecure)
+    db_password = "SuperSecretPassword123"
+    print("Accessing database with password:", db_password)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from the new version of Lambda! - security check test')
+        'body': 'Function executed successfully!'
     }

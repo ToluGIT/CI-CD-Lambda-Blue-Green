e9575e664866949e7ebf50be7e58b2aad2ab5507
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-11-06 20:18:56 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from the new version of Lambda!')
+        'body': json.dumps('Blue-green deployment update successful - v2')
     }
